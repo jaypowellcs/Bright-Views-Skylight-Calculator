c98fae432e925b50735b37c7656730181713ce6e
Jay Powell <jay.powellcs@gmail.com>
2022-06-26 12:58:56 -0500

    finished IdentifyScreen and GlassScreen

--- screens/GlassScreen.js
+++ screens/GlassScreen.js
@@ -1,11 +1,71 @@
-import { ScrollView, View } from 'react-native';
-import { Text, Card, Image, Button, Icon, Header } from 'react-native-elements';
+import { ScrollView, View, StyleSheet } from 'react-native';
+import { Text, Card, Image, Button, Icon, Header, ListItem } from 'react-native-elements';
+import deckmount from '../assets/images/deckmount.jpg';
+import curbmount from '../assets/images/curbmount.jpg';
+import selfflash from '../assets/images/selfflash.webp';
+
+const DetailGlassCard = () => {
+    return (
+        <Card style={{margin: 10}}>
+            <Card.Title>Glass Skylights</Card.Title>
+            <Card.Divider />
+            <Card.Image
+            style={{ padding: 0, height:250, width: 450 }}
+            source={deckmount}
+            />
+            <Text>
+            Benefits and Disadvantages of Glass Skylights
+            </Text>
+            <Card.Divider />
+            <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium ultrices elementum. Praesent at lorem sed diam consequat fringilla eget tincidunt eros. In interdum ante sed justo tincidunt interdum. Curabitur tincidunt urna non dignissim malesuada. Sed a aliquet quam. Phasellus et nunc tempus quam pulvinar dignissim sed eu odio. Cras tincidunt quam ac imperdiet dapibus. Sed euismod ante quis risus efficitur rhoncus. Proin odio nibh, tristique sed luctus a, aliquet eu mauris. In at euismod nisi, eu accumsan orci. Nulla eget porttitor tellus. Aenean scelerisque varius arcu sed tristique. Nullam sed libero laoreet, suscipit tortor a, dignissim justo. Vestibulum laoreet vestibulum elit quis pharetra. Sed sollicitudin augue erat, sed convallis mi mattis a.
+            </Text>
+
+        </Card>
+    );
+};
+
+const DetailAcrylicCard = () => {
+    return (
+        <Card style={{margin: 10}}>
+            <Card.Title>Acrylic Skylights</Card.Title>
+            <Card.Divider />
+            <Card.Image
+            style={{ padding: 0,  margin: 10 }}
+            source={selfflash}
+            />
+            <Text>
+            Benefits and Disadvantages of Acrylic Skylights 
+            </Text>
+            <Card.Divider />
+            <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pretium ultrices elementum. Praesent at lorem sed diam consequat fringilla eget tincidunt eros. In interdum ante sed justo tincidunt interdum. Curabitur tincidunt urna non dignissim malesuada. Sed a aliquet quam. Phasellus et nunc tempus quam pulvinar dignissim sed eu odio. Cras tincidunt quam ac imperdiet dapibus. Sed euismod ante quis risus efficitur rhoncus. Proin odio nibh, tristique sed luctus a, aliquet eu mauris. In at euismod nisi, eu accumsan orci. Nulla eget porttitor tellus. Aenean scelerisque varius arcu sed tristique. Nullam sed libero laoreet, suscipit tortor a, dignissim justo. Vestibulum laoreet vestibulum elit quis pharetra. Sed sollicitudin augue erat, sed convallis mi mattis a.
+            </Text>
+        </Card>
+    );
+};
+
+const styles = StyleSheet.create({
+    leftcontainer: {
+    marginBottom: 8,
+      flex: 1, 
+      flexDirection: 'column',
+    },
+    rightcontainer: {
+      marginBottom: 8,
+      flex: 2, 
+      flexDirection: 'column',
+    },
+});
 
 const GlassScreen = () => {
    return (
-   <Text>
-       yo
-   </Text>
+    <ScrollView>
+        <View>
+            <DetailGlassCard></DetailGlassCard>
+            <DetailAcrylicCard></DetailAcrylicCard>
+        </View>
+    </ScrollView>
    );
 };
 
--- screens/IdentifyScreen.js
+++ screens/IdentifyScreen.js
@@ -1,13 +1,111 @@
+import { ScrollView, View, StyleSheet } from 'react-native';
+import { Text, Card, Image, Button, Icon, Header } from 'react-native-elements';
+import deckmount from '../assets/images/deckmount.jpg';
+import curbmount from '../assets/images/curbmount.jpg';
+import selfflash from '../assets/images/selfflash.webp';
 
- import { ScrollView, View } from 'react-native';
- import { Text, Card, Image, Button, Icon, Header } from 'react-native-elements';
+
+const DeckMountCard = () => {
+    return (
+       
+                <Card  style={{margin: 10}}>
+                    <Card.Title>Deck Mounted Skylight</Card.Title>
+                    <Card.Divider />
+                    <Card.Image
+                    style={{ padding: 0, height:250, width: 450 }}
+                    source={deckmount}
+                    />
+                    <Text style={{ marginBottom: 10 }}>
+                    Is your Skylight a Deck Mounted Skylight? 
+                    </Text>
+                    <Button
+                    buttonStyle={{
+                        borderRadius: 0,
+                        marginLeft: 0,
+                        marginRight: 0,
+                        marginBottom: 0,
+                        backgroundColor: '#F5272B',
+                    }}
+                    title="Deck Mounted Calculator"
+                    />
+                </Card>
+    
+    );
+};
+
+const CurbMountCard = () => {
+    return (
+        
+                <Card>
+                    <Card.Title>Curb Mounted Skylight</Card.Title>
+                    <Card.Divider />
+                    <Card.Image
+                    style={{ padding: 0, height: 350,}}
+                    source={curbmount}
+                    />
+                    <Text style={{ marginBottom: 10 }}>
+                    Is your Skylight a Curb Mounted Skylight? 
+                    </Text>
+                    <Button
+                    buttonStyle={{
+                        borderRadius: 0,
+                        marginLeft: 0,
+                        marginRight: 0,
+                        marginBottom: 0,
+                        backgroundColor: '#F5272B',
+                    }}
+                    title="Curb Mounted Calculator"
+                    />
+                </Card>
+          
+    );
+};
+
+const SelfFlashCard = () => {
+    return (
+                <Card>
+                    <Card.Title>Self Flashed Skylight</Card.Title>
+                    <Card.Divider />
+                    <Card.Image
+                    style={{ padding: 0,  margin: 10 }}
+                    source={selfflash}
+                    />
+                    <Text style={{ marginBottom: 10 }}>
+                    Is your Skylight a Self Flashed Skylight? 
+                    </Text>
+                    <Button
+                    backgroundColor='#F5272B'
+                    buttonStyle={{
+                        borderRadius: 0,
+                        marginLeft: 0,
+                        marginRight: 0,
+                        marginBottom: 0,
+                        backgroundColor: '#F5272B',
+                    }}
+                    title="Self Flashed Calculator"
+                    />
+                </Card>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+        },
+    }
+);
+
 
 const IdentifyScreen = () => {
-    return (
-    <Text>
-        yo
-    </Text>
-    );
+   return (
+    <ScrollView>
+        <View style={styles.container}>
+            <DeckMountCard></DeckMountCard>
+            <CurbMountCard></CurbMountCard>
+            <SelfFlashCard></SelfFlashCard>
+        </View>
+    </ScrollView>
+   );
 };
 
 export default IdentifyScreen; 
